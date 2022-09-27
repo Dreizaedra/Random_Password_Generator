5d052d47ee2e24728f46092a0b06021ea339f819
DeuRez <114523070+Dreizaedra@users.noreply.github.com>
2022-09-28 01:02:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ generate_passwords_btn.addEventListener("click", function() {
         message_el.innerHTML = `
             Type the number of characters you want<br />
             your password to have (8 to 18)<br />
-            <span class='bold text-green'>Baseline : 13</span>
+            <span class="bold text-green">Baseline : 13</span>
         `;
         user_input.value = 13;
     };
